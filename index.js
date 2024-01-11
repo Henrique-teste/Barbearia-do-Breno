138cad69e5f693b70fa9364a831c1e1644141b7a
Henrique-teste <152813884+Henrique-teste@users.noreply.github.com>
2024-01-11 09:06:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,3 @@
 $(document).ready(function(){
-})
-$('form button').click(function(){
-    $('thead th').slideDown();
-})
-$('form button').click(function(){
-    $('tfoot th').slideDown();
-})
-$('form').on('submit',function(e){
-    e.preventDefault();
-    const nomeCliente = $('#nome-cliente').val();
-    const numeroCliente = $('#telefone-cliente').val();
-    const emailCliente = $('#email-cliente').val();
-    const dataCliente = $('#horario-cliente').val();
-    const infoClientes = $('<tr></tr>')
-    $(`                
-    <tr>
-        <td>
-        ${nomeCliente} -
-        ${dataCliente}
-        ${emailCliente} -
-        ${numeroCliente}
-        </td> 
-    </tr>
-    `).appendTo(infoClientes)
-    $(infoClientes).appendTo('tbody')
-        $('#nome-cliente').val('');
-    $('#telefone-cliente').val('');
-    $('#email-cliente').val('');
-    $('#horario-cliente').val('');
     
 })
